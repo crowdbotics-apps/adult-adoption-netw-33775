06c080622218d5d7f7bb9f047be8683f9b7eb9fa
Crowdbotics <team@crowdbotics.com>
2022-02-21 22:21:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/adult_adoption_netw_33775/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adult_adoption_netw_33775;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "adult_adoption_netw_33775";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "adult_adoption_netw_33775",
+  url: "https://adult_adoption_netw_33775.botics.co",
+  api: "https://adult_adoption_netw_33775.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
